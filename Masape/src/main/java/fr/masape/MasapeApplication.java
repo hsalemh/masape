014a36bfd3b04538a7afa75d9810c2095bafdd59
salem <salem@192.168.1.42>
2022-05-20 11:15:15 +0200

    voici les bases du projet masape ! bon courage à tous

---
+++ Masape/src/main/java/fr/masape/MasapeApplication.java
@@ -0,0 +1,13 @@
+package fr.masape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasapeApplication.class, args);
+	}
+
+}
---
+++ Masape/src/test/java/fr/masape/MasapeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.masape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
