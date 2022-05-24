6b8877909995f48a465c5b6ce0a5f9d306c02bb9
Ismaila SACKO <ismalsacko@yahoo.fr>
2022-05-24 11:24:44 +0200

    Test des entités dans la classe Main

--- Masape/src/main/java/fr/masape/MasapeApplication.java
+++ Masape/src/main/java/fr/masape/MasapeApplication.java
@@ -3,6 +3,8 @@ package fr.masape;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fr.masape.entities.Commande;
+import fr.masape.entities.Produit;
 import fr.masape.entities.User;
 
 @SpringBootApplication
@@ -14,7 +16,12 @@ public class MasapeApplication {
 		
 		User u = new User("SACKO", "Ismaila", "ismalsacko@yahoo.fr", "00 00 56 56 89", "Châtinay-Malabey");
 
-
+		Produit p = new Produit("Paton", 75.00);
+		
+		Commande c = new Commande();
+		System.out.println(u);
+		System.out.println();
+		System.out.println(p);
 	}
 	
 }
